622fbe5489b29157e733f15e284a1eca5f6ebf1d
Robi Adadi Suparlan <robiadadis@gmail.com>
2024-10-21 09:42:59 +0700

    update ui and customize rainbowkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,23 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import merge from 'lodash.merge';
+import { getDefaultWallets, RainbowKitProvider, darkTheme, Theme,   } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import {
-  sepolia,
-  polygonMumbai,
-} from "wagmi/chains";
+import { sepolia } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import MainLayout from "../layout/mainLayout";
 
 const { chains, provider } = configureChains(
   [
-    sepolia,
-    polygonMumbai,
+    sepolia
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My DApp",
-  projectId: "your-project-id", // Ganti dengan project ID Anda
+  appName: "ethervote",
+  projectId: "8d419e5e746df5332cd772ec6294d8c4",
   chains,
 });
 
@@ -30,19 +27,76 @@ const wagmiClient = createClient({
   provider,
 });
 
+const myTheme = merge(darkTheme(), {
+  blurs: {
+    modalOverlay: 'small',
+  },
+  colors: {
+    accentColor: '#181818',
+    accentColorForeground: 'white',
+    // actionButtonBorder: '...',
+    // actionButtonBorderMobile: '...',
+    // actionButtonSecondaryBackground: '...',
+    closeButton: 'white',
+    // closeButtonBackground: '...',
+    connectButtonBackground: '#f9fafb',
+    // connectButtonBackgroundError: '...',
+    connectButtonInnerBackground: '#fce7f3',
+    connectButtonText: '#181818',
+    // connectButtonTextError: '...',
+    // connectionIndicator: '...',
+    // downloadBottomCardBackground: '...',
+    // downloadTopCardBackground: '...',
+    // error: '...',
+    // generalBorder: '...',
+    // generalBorderDim: '...',
+    // menuItemBackground: '...',
+    // modalBackdrop: '...',
+    // modalBackground: '...',
+    // modalBorder: '...',
+    modalText: 'white',
+    // modalTextDim: '...',
+    // modalTextSecondary: '...',
+    // profileAction: '...',
+    // profileActionHover: '...',
+    // profileForeground: '...',
+    // selectedOptionBorder: '...',
+    // standby: '...',
+  },
+  fonts: {
+    body: 'DM Sans, sans-serif',
+  },
+  radii: {
+    actionButton: 'none',
+    connectButton: 'none',
+    menuButton: 'none',
+    modal: 'none',
+    modalMobile: 'none',
+  },
+  // shadows: {
+  //   connectButton: '...',
+  //   dialog: '...',
+  //   profileDetailsAction: '...',
+  //   selectedOption: '...',
+  //   selectedWallet: '...',
+  //   walletLogo: '...',
+  // },
+});
+
 export { WagmiConfig, RainbowKitProvider };
 
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider
-        modalSize="compact"
+        modalSize="wide"
         initialChain={sepolia}
         chains={chains}
+        theme={myTheme}
       >
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
       </RainbowKitProvider>
     </WagmiConfig>
   );
